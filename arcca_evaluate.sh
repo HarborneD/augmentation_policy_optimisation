1de09245b8fc72f924365bff4363936cd0fdaab3
harborned <d.harborne@outlook.com>
2019-02-10 01:50:19 +0000

    arc script update 3

--- arcca_evaluate.sh
+++ arcca_evaluate.sh
@@ -16,7 +16,11 @@
 #SBATCH --ntasks-per-node=1
 
 module load CUDA/9.1
+module load python-numpy
+module load python-h5py
 module load tensorflow
+
+pip install --user keras
 
 MODEL_NAME="wrn"
 CHECKPOINT_DIR="$(pwd)/checkpoints_$1"
