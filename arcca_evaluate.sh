9e8edfe5093449e43dec30532868c1a5b888d5a3
harborned <d.harborne@outlook.com>
2019-02-10 01:31:37 +0000

    arcca evalue script

---
+++ arcca_evaluate.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+#SCW_TPN_OVERRIDE=1 #overide warning about 1 node
+# BASH Environment Variable	           SBATCH Field Code	Description
+# $SLURM_JOB_ID	                        %J	                Job identifier
+# $SLURM_ARRAY_JOB_ID	                %A	                Array parent job identifier
+# $SLURM_ARRAY_TASK_ID	                %a	                Array job iteration index
+
+#SBATCH --gres=gpu:2
+#SBATCH -p gpu
+
+#SBATCH -J tensorflowTest
+#SBATCH -o output-%J.o
+#SBATCH -n 1
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+
+module load CUDA/9.1
+module load tensorflow
+
+MODEL_NAME="wrn"
+CHECKPOINT_DIR="$(pwd)/checkpoints_$1"
+DATA_PATH="/home/c.c0919382/datasets/cifar-10-batches-py"
+DATASET="cifar10"
+
+python2 -m evaluator.evaluate_policies.py \
+    --model_name=$MODEL_NAME \
+    --checkpoint_dir=$CHECKPOINT_DIR \
+    --data_path $DATA_PATH \
+    --dataset=$DATASET \
+    --use_cpu=0 \
+    --policy_id=$1 \
+    --num_epochs=$2
+
