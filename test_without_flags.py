8b010a387b3e31de577a97cf0a96bcb6a16adb22
harborned <d.harborne@outlook.com>
2019-02-11 13:21:05 +0000

    working local genetic alg

---
+++ test_without_flags.py
@@ -0,0 +1,50 @@
+import tensorflow as tf
+import evaluator.evaluate_policies_without_flags
+import os 
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+
+
+tf.flags.DEFINE_string('model_name', "wrn",
+                    'wrn, shake_shake_32, shake_shake_96, shake_shake_112, '
+                    'pyramid_net')
+tf.flags.DEFINE_string('checkpoint_dir', "default_checkpoints", 'Training Directory.')
+tf.flags.DEFINE_string('data_path', "/data/cifar10",
+                    'Directory where dataset is located.')
+tf.flags.DEFINE_string('dataset', "cifar10",
+                    'Dataset to train with. Either cifar10 or cifar100')
+tf.flags.DEFINE_integer('use_cpu', 0, '1 if use CPU, else GPU.')
+tf.flags.DEFINE_string('policy_id', "default_policy_0001", 'id of policy to be evaluated')
+tf.flags.DEFINE_integer('num_epochs', 2, 'Number of epochs to train model before evaluating')
+
+FLAGS = tf.flags.FLAGS
+
+def TrainWithPolicy(policy_id, num_epochs, data_path, dataset="cifar10", model_name="wrn", use_cpu=0):
+    print("Training with policy:"+str(policy_id))
+    checkpoint_dir = os.path.join(os.getcwd(),"checkpoints_"+policy_id)
+
+    FLAGS.model_name = model_name
+    FLAGS.checkpoint_dir = checkpoint_dir 
+    FLAGS.data_path = data_path 
+    FLAGS.dataset = dataset 
+    FLAGS.use_cpu = use_cpu 
+    FLAGS.policy_id = policy_id 
+    FLAGS.num_epochs = num_epochs 
+
+    valid_accuracy, test_accuracy = evaluator.evaluate_policies_without_flags.TrainModelWithPolicies(FLAGS)
+    return valid_accuracy, test_accuracy
+
+
+if __name__ == "__main__":
+    policy_id = "000001"
+    num_epochs = 20
+    model_name = "wrn"
+    data_path = "/media/harborned/ShutUpN/datasets/cifar/cifar-10-batches-py"
+    dataset = "cifar10"
+    use_cpu = 0 
+
+    print("Training Child Model Using Policy: "+str(policy_id))
+    valid_accuracy, test_accuracy = TrainWithPolicy(policy_id, num_epochs, data_path, dataset="cifar10", model_name="wrn", use_cpu=0)
+
+    print("valid_accuracy", "test_accuracy")
+    print(valid_accuracy, test_accuracy)
