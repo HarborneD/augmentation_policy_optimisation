aa4781d43058adea4b78c15e634449af5475c0be
harborned <d.harborne@outlook.com>
2019-02-11 13:33:11 +0000

    More test scripts added, python scripts now take data path arg

---
+++ ArccaGeneticAugment.sh
@@ -0,0 +1,30 @@
+
+
+#!/bin/sh --login
+
+#SCW_TPN_OVERRIDE=1 #overide warning about 1 node
+# BASH Environment Variable	           SBATCH Field Code	Description
+# $SLURM_JOB_ID	                        %J	                Job identifier
+# $SLURM_ARRAY_JOB_ID	                %A	                Array parent job identifier
+# $SLURM_ARRAY_TASK_ID	                %a	                Array job iteration index
+
+#SBATCH --gres=gpu:2
+#SBATCH -p gpu
+
+#SBATCH --job-name=eval_child_model
+#SBATCH -o output-%J.o
+#SBATCH -n 1
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+
+module load CUDA/9.1
+echo now load TF
+module load tensorflow
+echo tf loaded
+
+MODEL_NAME="wrn"
+CHECKPOINT_DIR="$(pwd)/checkpoints_$1"
+DATA_PATH="/home/c.c0919382/datasets/cifar-10-batches-py"
+DATASET="cifar10"
+
+python -m GeneticAugment $DATA_PATH
