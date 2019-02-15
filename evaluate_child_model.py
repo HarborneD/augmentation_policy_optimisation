81426b94c31aaeea945631e4a5399e4542d97a96
harborned <d.harborne@outlook.com>
2019-02-15 01:10:24 +0000

    getopt update

--- evaluate_child_model.py
+++ evaluate_child_model.py
@@ -69,8 +69,9 @@ if __name__ == "__main__":
     print("Argv:")
     print(sys.argv)
     print("")
+    
     try:
-      opts, args = getopt.getopt(sys.argv[1:],"d:p:e:m:t:c:",["data_path=","policy_id=","num_epochs=","model_name","dataset","use_cpu"])
+      opts, args = getopt.getopt(sys.argv[1:],"d:p:e:mtc",["data_path=","policy_id=","num_epochs=","model_name","dataset","use_cpu"])
     
     except getopt.GetoptError:
       print('evaluate_child_model.py --data_path <data_path> --policy_id <policy_id> --num_epochs <num_epochs> --model_name <model_name> --dataset <dataset> --use_cpu <use_cpu>')
